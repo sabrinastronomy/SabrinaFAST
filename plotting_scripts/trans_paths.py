4620a4caa4c547047bc1caa125eab1947740bfc8
Sabrina Berger <sabrinaberger@paper.pvt>
2018-02-16 14:21:26 -0800

    organized & new initial plotting started

---
+++ plotting_scripts/trans_paths.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+#
+# Heimdall python module for path inforamtion
+#
+
+def getControlDir():
+  return "/home/nch/linux_64/control"
+
+def getConfigDir():
+  return "/home/nch/linux_64/share"
+
+def getBinaryDir():
+  return "/home/nch/linux_64/bin"
+
