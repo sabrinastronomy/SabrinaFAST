11e9cdf5d238a2ea8b19f20daf6d5c525ba13386
Sabrina Berger <sabrinaberger@berkeley.edu>
2018-02-16 15:03:41 -0800

    indenting should just be universal

--- FAST_PLOTS.py
+++ FAST_PLOTS.py
@@ -1,67 +1,70 @@
 #!/usr/bin/python
 
-import subprocess
 import os
-
+import subprocess
 
 # Change when you have information about where filterbank/candidate files will be stored for each beam
 data_directory_default = '/home/sabrinaberger/FAST/test_candidates/'
 plot_directory_default = '/home/sabrinaberger/FAST/test_plots/'
 
 
-
 class plot:
-    """
-  Parameters taken by trans_gen_overview.py (if included *):
-  *-cands_file CANDS_FILE 
-  *-nbeams NBEAMS 
-  *-snr_cut SNR_CUT
-  *-beam_mask BEAM_MASK
-  *-nbeams_cut NBEAMS_CUT
-  -members_cut MEMBERS_CUT
-  -dm_cut DM_CUT
-  *-filter_cut FILTER_CUT
-  -filter_max FILTER_MAX
-  -min_bins MIN_BINS
-  -resolution RESOLUTION
-  -std_out
-  -skip_rows SKIP_ROWS
-  -just_time_dm
-  -cand_list_xml
-  -max_cands MAX_CANDS
-  -no_plot
-  -interactive
-  -verbose
-    """
-	def __init__(self, all_cands_file = "*_all.cand", nbeams = 19, snr_cut=6, beam_mask = 65536, nbeams_cut=4, filter_cut=16, multibeam = False, data_directory = data_directory_default, plot_directory = plot_directory_default):
-		self.all_cands_file = all_cands_file
-		self.nbeams = nbeams
-		self.snr_cut = snr_cut
-		self.beam_mask = beam_mask
-		self.nbeams_cut = nbeams_cut
-		self.filter_cut = filter_cut
-		self.multibeam = multibeam
-		self.data_directory = data_directory
-		self.plot_directory = plot_directory
+    def __init__(self, all_cands_file="*_all.cand", nbeams=19, snr_cut=6, beam_mask=65536, nbeams_cut=4, filter_cut=16,
+                 multibeam=False, data_directory=data_directory_default, plot_directory=plot_directory_default):
 
-	def make_overview_plot():
-		# Directory already exists
-		if (os.path.isdir(plot_directory)):
-			subprocess.call('cd ' + plot_directory, shell=True)
+        """
+          Parameters taken by trans_gen_overview.py (if included *):
+          *-cands_file CANDS_FILE
+          *-nbeams NBEAMS
+          *-snr_cut SNR_CUT
+          *-beam_mask BEAM_MASK
+          *-nbeams_cut NBEAMS_CUT
+          -members_cut MEMBERS_CUT
+          -dm_cut DM_CUT
+          *-filter_cut FILTER_CUT
+          -filter_max FILTER_MAX
+          -min_bins MIN_BINS
+          -resolution RESOLUTION
+          -std_out
+          -skip_rows SKIP_ROWS
+          -just_time_dm
+          -cand_list_xml
+          -max_cands MAX_CANDS
+          -no_plot
+          -interactive
+          -verbose
+        """
 
-		else:
-		# Make directory to save plots to 
-			subprocess.call('mkdir CandidatePlots', shell=True)
-			subprocess.call('cd CandidatePlots', shell=True)
+        self.all_cands_file = all_cands_file
+        self.nbeams = nbeams
+        self.snr_cut = snr_cut
+        self.beam_mask = beam_mask
+        self.nbeams_cut = nbeams_cut
+        self.filter_cut = filter_cut
+        self.multibeam = multibeam
+        self.data_directory = data_directory
+        self.plot_directory = plot_directory
 
-		# Make overview plot - input: *all.cand file
-		subprocess.call('python trans_gen_overview.py -cands_file {all_cand} -nbeam {nbeams} -snr_cut {snr_cut} -beam_mask {beam_mask} -nbeams_cut {nbeams_cut} -filter_cut {filter_cut}'
-			.format(all_cand=self.all_cands_file, nbeams=self.nbeams, snr_cut=self.snr_cut, beam_mask=self.beam_mask, nbeams_cut=self.nbeams_cut, filter_cut=self.filter_cut))
+    def make_overview_plot(self):
+        # Directory already exists
+        if (os.path.isdir(self.plot_directory)):
+            subprocess.call('cd ' + self.plot_directory, shell=True)
 
-if __name__ = "__main__":
+        else:
+            # Make directory to save plots to
+            subprocess.call('mkdir CandidatePlots', shell=True)
+            subprocess.call('cd CandidatePlots', shell=True)
 
-	testing_plot = plot()
-	testing_plot.make_overview_plot
+        # Make overview plot - input: *all.cand file
+        subprocess.call(
+            'python trans_gen_overview.py -cands_file {all_cand} -nbeam {nbeams} -snr_cut {snr_cut} -beam_mask {beam_mask} -nbeams_cut {nbeams_cut} -filter_cut {filter_cut}'
+            .format(all_cand=self.all_cands_file, nbeams=self.nbeams, snr_cut=self.snr_cut, beam_mask=self.beam_mask,
+                    nbeams_cut=self.nbeams_cut, filter_cut=self.filter_cut))
+
+
+if __name__ == "__main__":
+    testing_plot = plot()
+    testing_plot.make_overview_plot()
 
 
 
