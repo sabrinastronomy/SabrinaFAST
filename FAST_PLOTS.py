abb4d481681fdaf4450ef81ca8e6beb8eac1113a
Sabrina Berger <sabrinaberger@paper.pvt>
2018-03-07 14:11:38 -0800

    trans gen working now

--- FAST_PLOTS.py
+++ FAST_PLOTS.py
@@ -3,13 +3,32 @@
 import os
 import subprocess
 
+import os
+
+class cd:
+    """
+       Context manager for changing the current working directory
+       Thank you, Brian M. Hunt from StackOverflow.
+    """
+    
+    def __init__(self, newPath):
+        self.newPath = os.path.expanduser(newPath)
+
+    def __enter__(self):
+        self.savedPath = os.getcwd()
+        os.chdir(self.newPath)
+
+    def __exit__(self, etype, value, traceback):
+        os.chdir(self.savedPath)
+
+
 # Change when you have information about where filterbank/candidate files will be stored for each beam
-data_directory_default = '/home/sabrinaberger/FAST/test_candidates/'
+data_directory_default = '/home/sabrinaberger/FAST/test_files/'
 plot_directory_default = '/home/sabrinaberger/FAST/CandidatePlots/'
 
 
 class plot:
-  def __init__(self, all_cands_file="*_all.cand", nbeams=19, snr_cut=6, beam_mask=65536, nbeams_cut=4, filter_cut=16,
+  def __init__(self, all_cands_file=data_directory_default + "*_all.cand", nbeams=19, snr_cut=6, beam_mask=65536, nbeams_cut=4, filter_cut=16,
                multibeam=False, data_directory=data_directory_default, plot_directory=plot_directory_default):
 
     """
@@ -46,23 +65,19 @@ class plot:
     self.plot_directory = plot_directory
 
   def make_overview_plot(self):
-    # Directory already exists
-    if (os.path.isdir(self.plot_directory)):
-      subprocess.call('cd ' + self.plot_directory, shell=True)
+    if not (os.path.isdir(self.plot_directory)):
+       subprocess.call('mkdir CandidatePlots', shell=True)
 
-    else:
-        # Make directory to save plots to
-      subprocess.call('mkdir CandidatePlots', shell=True)
-      subprocess.call('cd CandidatePlots', shell=True)
-
+    with cd(self.plot_directory):
     # TO DO make this better string
     # Make overview plot - input: *all.cand file
-    nplot_call = 'python /home/sabrinaberger/FAST/plotting_scripts/trans_gen_overview.py -cands_file ' + self.all_cands_file + ' -nbeam ' +  str(self.nbeams) + ' -snr_cut ' + str(self.snr_cut) + ' -beam_mask ' + str(self.beam_mask) + ' -nbeams_cut ' + str(self.nbeams_cut) + ' -filter_cut ' + str(self.filter_cut)
-    subprocess.call(nplot_call, shell=True)
+    #nplot_call = 'python /home/sabrinaberger/FAST/plotting_scripts/trans_gen_overview.py -cands_file ' + self.all_cands_file + ' -nbeams ' +  str(self.nbeams) + ' -snr_cut ' + str(self.snr_cut) + ' -beam_mask ' + str(self.beam_mask) + ' -nbeams_cut ' + str(self.nbeams_cut) + ' -filter_cut ' + str(self.filter_cut)
+       nplot_call = 'python /home/sabrinaberger/FAST/plotting_scripts/trans_gen_overview.py -cands_file ' + self.all_cands_file + ' -nbeams ' +  str(self.nbeams) + ' -snr_cut ' + str(self.snr_cut) + ' -nbeams_cut ' + str(self.nbeams_cut) + ' -filter_cut ' + str(self.filter_cut)
+       subprocess.call(nplot_call, shell=True)
 
 
 if __name__ == "__main__":
-  testing_plot = plot()
+  testing_plot = plot(data_directory_default + '2001-01-25-08:32:40_all.cand')
   testing_plot.make_overview_plot()
 
 
